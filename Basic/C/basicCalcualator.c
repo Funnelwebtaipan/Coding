f110ae623653e6b11c8004d30ad773602bbcf00f
Funnelwebtaipan <funnelwebtaipan@protonmail.com>
2025-07-15 21:42:47 +1200

    Updated basicCalcualator.c to Basic/C

--- Basic/C/basicCalcualator.c
+++ Basic/C/basicCalcualator.c
@@ -35,14 +35,15 @@ float pi_radius (int x)
 int main ()
 {
     int choice, x, y;
-    printf("This is basic calculator program\n");
-    printf("Enter in the calculator operation:\n");
-    printf("1. Addition\n");
-    printf("2. Subtraction\n");
-    printf("3. Multiplication\n");
-    printf("4. Division\n");
-    printf("5. Pi Radius\n");
-    printf("8. Exit\n");
+    printf("\nThis is basic calculator program\n");
+    printf("\n1. Addition\n");
+    printf("\n2. Subtraction\n");
+    printf("\n3. Multiplication\n");
+    printf("\n4. Division\n");
+    printf("\n5. Pi Radius\n");
+    printf("\n8. Exit\n");
+    printf("\nEnter in the calculator operation:\n");
+
     scanf("%d", &choice);
 
 
@@ -50,47 +51,48 @@ int main ()
     switch (choice)
     {
         case 1:
-            printf("You have selected Addition\n");
-            printf("Enter two numbers\n");
+            printf("\nYou have selected Addition\n");
+            printf("\nEnter two numbers\n");
             scanf("%d%d", &x, &y);
-            printf("The sum of the values are %f\n", additionf(x,y));
+            printf("\nThe sum of the values are %f\n", additionf(x,y));
             break;
         case 2:
-            printf("You have selected Subtraction\n");
-            printf("Enter two numbers:\n");
-            scanf("%d%d", &x, &y);
-            printf("The sum of the two values are:\n", subtractionf(x,y));
+            printf("\nYou have selected Subtraction\n");
+            printf("\nEnter two numbers:\n");
+            scanf("\n%d%d", &x, &y);
+            printf("\nThe sum of the two values are:\n", subtractionf(x,y));
             break;
         case 3:
-            printf("You have selected Multiplication\n");
-            printf("Enter two numbers:\n");
-            scanf("%d%d", &x, &y);
-            printf("The sum of the two values are %f\n", multiplicationf(x,y));
+            printf("\nYou have selected Multiplication\n");
+            printf("\nEnter two numbers:\n");
+            scanf("\n%d%d", &x, &y);
+            printf("\nThe sum of the two values are %f\n", multiplicationf(x,y));
             break;
         case 4:
-            printf("You have selected Division\n");
-            printf("Enter two values:\n");
-            scanf("%d%d", &x, &y);
+            printf("\nYou have selected Division\n");
+            printf("\nEnter two values:\n");
+            scanf("\n%d%d", &x, &y);
             if (y == 0)
             {
-                printf("Cannot divide by zero\n");
+                printf("\nCannot divide by zero\n");
                 break;
             }
-            printf("The sum of the two values are:%f\n", divisionf(x,y));
+            printf("\nThe sum of the two values are:%f\n", divisionf(x,y));
             break;
         case 5:
-            printf("You have selected Pi Radius\n");
-            printf("Enter in the radius value:\n");
-            scanf("%d", &x);
-            printf("The radius is %f\n", pi_radius(x));
+            printf("\nYou have selected Pi Radius\n");
+            printf("\nEnter in the radius value:\n");
+            scanf("\n%d", &x);
+            printf("\nThe radius is %f\n", pi_radius(x));
             break;
         case 8:
-            printf("You have selected to exit the program\n");
+            printf("\nYou have selected to exit the program\n");
             break;
         default:
-            printf("Invalid choice\n");
+            printf("\nInvalid choice\n");
             break;
     }
+    printf("\nThank you for playing\n");
     return 0;
 
 }
